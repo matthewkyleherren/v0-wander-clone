320e19072838a5775964d4e216464ed8cf7c4c30
matthewkyleherren <99541432+matthewkyleherren@users.noreply.github.com>
2026-01-12 12:17:12 +0100

    lknl

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,16 +1,16 @@
 /**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
-import { defineCliConfig } from 'sanity/cli'
+ * This configuration file lets you run `$ sanity [command]` in this folder
+ * Go to https://www.sanity.io/docs/cli to learn more.
+ **/
+import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({
-  api: { projectId, dataset }
+  api: { projectId, dataset },
   deployment: {
-      appId: 'eudfg78mw284ohkrc5wxjwci',
-      autoUpdates: true
-    }
-})
+    appId: "eudfg78mw284ohkrc5wxjwci",
+    autoUpdates: true,
+  },
+});
